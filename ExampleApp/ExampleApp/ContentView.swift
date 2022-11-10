ad8b338af5c5dcb0b28a5fd0a0c047be0b6fa355
Marco Eidinger <eidingermarco@gmail.com>
2022-11-09 17:16:53 -0800

    use ExamplePackageTarget in app

--- ExampleApp/ExampleApp/ContentView.swift
+++ ExampleApp/ExampleApp/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ExamplePackageTarget
 
 struct ContentView: View {
     var body: some View {
@@ -13,7 +14,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(ExamplePackage().text)
         }
         .padding()
     }
