99914f5d998900dd246e2777cbbcb347883b6030
Marco Eidinger <eidingermarco@gmail.com>
2022-11-09 17:55:06 -0800

    synchronous implementation of echo command-line tool using ArgumentParser

--- echo/Sources/echo/echo.swift
+++ echo/Sources/echo/echo.swift
@@ -1,8 +1,18 @@
-@main
-public struct echo {
-    public private(set) var text = "Hello, World!"
+import ArgumentParser
 
-    public static func main() {
-        print(echo().text)
+@main
+struct Echo: ParsableCommand {
+    @Option(name: .shortAndLong, help: "The number of times to echo 'phrase'.")
+    var count: Int?
+
+    @Argument(help: "The phrase to echo.")
+    var phrase: String
+
+    mutating func run() throws {
+        let repeatCount = count ?? 1
+
+        for _ in 1...repeatCount {
+            print(phrase)
+        }
     }
 }
