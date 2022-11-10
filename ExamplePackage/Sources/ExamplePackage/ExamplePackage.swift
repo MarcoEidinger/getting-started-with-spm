beabca4df935f043bcc83473f635e60fa21d9a6f
Marco Eidinger <eidingermarco@gmail.com>
2022-11-09 17:26:57 -0800

    example of using compiler directive

--- ExamplePackage/Sources/ExamplePackage/ExamplePackage.swift
+++ ExamplePackage/Sources/ExamplePackage/ExamplePackage.swift
@@ -7,9 +7,20 @@ public struct ExamplePackage {
     }
 }
 
+@available(iOS 15.0, *)
 public struct ReusableView: View {
     public init() {}
     public var body: some View {
         Text(ExamplePackage().text)
+#if os(iOS)
+            .foregroundColor(Color(uiColor: .red))
+#endif
+    }
+}
+
+@available(iOS 15.0, *)
+struct ReusableView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReusableView()
     }
 }
