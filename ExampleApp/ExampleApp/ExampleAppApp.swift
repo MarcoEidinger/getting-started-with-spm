129cc2b819e6099cb91dee05107a42e4f9fed3cc
Marco Eidinger <eidingermarco@gmail.com>
2022-11-09 17:11:48 -0800

    Create ExampleApp (iOS) from Xcode

---
+++ ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Marco Eidinger on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
