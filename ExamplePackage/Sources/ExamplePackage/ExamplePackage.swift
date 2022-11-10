d6d565ff020c55401da91bd295b9614a9ddcab03
Marco Eidinger <eidingermarco@gmail.com>
2022-11-09 17:19:20 -0800

    create ReusableView in package (@available)

--- ExamplePackage/Sources/ExamplePackage/ExamplePackage.swift
+++ ExamplePackage/Sources/ExamplePackage/ExamplePackage.swift
@@ -1,6 +1,15 @@
+import SwiftUI
+
 public struct ExamplePackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+}
+
+@available(iOS 13.0, *)
+struct ReusableView: View {
+    var body: some View {
+        Text(ExamplePackage().text)
     }
 }
