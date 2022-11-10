ee042b20cad011b113b593a95862508519ea9f44
Marco Eidinger <eidingermarco@gmail.com>
2022-11-09 18:03:06 -0800

    rename executable to "simonsays" and create install/uninstall scripts

--- echo/Package.swift
+++ echo/Package.swift
@@ -3,8 +3,9 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "echo",
+let package: Package = Package(
+    name: "simonsays",
+    platforms: [.macOS(.v10_15)],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.0"),
@@ -13,7 +14,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
-            name: "echo",
+            name: "simonsays",
             dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
     ]
 )
--- echo/Sources/simonsays/echo.swift
+++ echo/Sources/simonsays/echo.swift
@@ -1,18 +1,23 @@
 import ArgumentParser
 
 @main
-struct Echo: ParsableCommand {
+struct SimonSays: AsyncParsableCommand {
     @Option(name: .shortAndLong, help: "The number of times to echo 'phrase'.")
     var count: Int?
 
     @Argument(help: "The phrase to echo.")
     var phrase: String
 
-    mutating func run() throws {
+    mutating func run() async throws {
         let repeatCount = count ?? 1
 
         for _ in 1...repeatCount {
-            print(phrase)
+            await delay(echo: phrase)
         }
     }
+    
+    func delay(echo: String) async -> Void {
+            try? await Task.sleep(nanoseconds: 1_000_000_000)
+            print(echo)
+        }
 }
